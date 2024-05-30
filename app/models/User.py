ba830746aceaea376527e8018ff7092e047c6fb5
Valentin Lestrelin <valentin.lestrelin@centrale-med.fr>
2024-05-30 13:50:59 +0200

    Correction user model

--- app/models/User.py
+++ app/models/User.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128), nullable=True)
+    password = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.login)
